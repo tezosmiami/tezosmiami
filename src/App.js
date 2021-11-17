3b7276e738ca8ab1eee53dfb91933ae0ca1a537a
Nicolask27 <nick.kreimer@qlx.com>
2021-11-16 23:26:04 -0500

    First Change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './TezosLogo.png';
+import bkground from './Miami.jpeg';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+   <img src={bkground}></img>
+   <img src={logo} class="image1"></img>
+   </div>
+   );
 }
 
 export default App;
