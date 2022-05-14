97cd219d6c8999618ad83cf87d96fbfbee80a2a7
tezosmiami <tezosmiami@gmail.com>
2022-05-14 13:27:22 -0400

    clean up. . .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import logo from './TezosLogo.png';
-import bkground from './dusk.jpeg';
 import "./App.css";
 import twitter from './twitter.png';
 import fruitensalda from './fruitensalada.jpg';
@@ -18,57 +17,51 @@ export const App = () => {
 
   return (
     <div>
-    
-   {/* <img src={bkground}  className='back' alt="Miami"></img> */}
-
-  <div>
-  <a href="https://tezos.com" target="blank" rel="noopener noreferrer">
-   <img src={logo} className="image1" alt="Tezos"></img></a>
-   </div>
-   <div style={{marginTop: '22px'}}>
-    {sites.map((i) => {
-        return <div className='sites' key={i.name}>
-                  <a href={i.link} target="blank" rel="noopener noreferrer">{[i.icon,' ',i.name, ' ',i.icon]}</a>
-              </div>
-      }
-    )}
-
-</div>
-  <div>
-{/* <a href="https://tezos.com" target="blank" rel="noopener noreferrer">
-   <img src={logo} className="image1" alt="Tezos"></img></a> */}
-
-   </div>
-  <div className="logo">
-   
-  <a href="https://twitter.com/tezosmiami" target="blank" rel="noopener noreferrer"><img src={twitter} className="twitter" alt="Twitter"/>
-  </a>
-  <a href="https://hen.miami/tezosmiami" target="blank" rel="noopener noreferrer">
-  <svg styles="logo" viewBox="0 0 196.87 53.23" fill="#000000">
-                  <path
-                    d="M228.9,79.31H211.51a2.26,2.26,0,0,1-.35-.34.75.75,0,0,1-.16-.42c0-11.42,0-22.85,0-34.43H193.24v35H175.41V26.27H228.9Z"
-                    transform="translate(-32.03 -26.27)"
-                  />
-                  <path
-                    d="M67.74,43.78V26.42H85.41V79.19H67.91V62.38a4.24,4.24,0,0,0-.52-.57.77.77,0,0,0-.42-.17H50V79.08H32V26.48H49.78v17.3Z"
-                    transform="translate(-32.03 -26.27)"
-                  />
-                  <path
-                    d="M103.62,43.79V26.43h53.6c.09,5.62,0,11.41.05,17.36Z"
-                    transform="translate(-32.03 -26.27)"
-                  />
-                  <path
-                    d="M103.71,61.71h53.38V78.84c-4.05.69-38.16.91-53.38.31Z"
-                    transform="translate(-32.03 -26.27)"
-                  />
-                </svg>
-       </a>    
-  <a href="https://fruitensalada.xyz" target="blank" rel="noopener noreferrer">    
-  <img className="fruta" src={fruitensalda} alt="fruitensalda"/></a>
-  </div>
+      <div>
+        <a href="https://tezos.com" target="blank" rel="noopener noreferrer">
+        <img src={logo} className="image1" alt="Tezos"></img></a>
+      </div>
   
-   </div>
+      <div style={{marginTop: '22px'}}>
+        {sites.map((i) => {
+            return <div className='sites' key={i.name}>
+                      <a href={i.link} target="blank" rel="noopener noreferrer">{[i.icon,' ',i.name, ' ',i.icon]}</a>
+                  </div>
+          }
+        )}
+      </div>
+      <div />
 
+      <div className="logo">
+        <a href="https://twitter.com/tezosmiami" target="blank" rel="noopener noreferrer">
+          <img src={twitter} className="twitter" alt="Twitter"/>
+        </a> 
+        <a href="https://hen.miami/tezosmiami" target="blank" rel="noopener noreferrer">
+          <svg styles="logo" viewBox="0 0 196.87 53.23" fill="#000000">
+            <path
+              d="M228.9,79.31H211.51a2.26,2.26,0,0,1-.35-.34.75.75,0,0,1-.16-.42c0-11.42,0-22.85,0-34.43H193.24v35H175.41V26.27H228.9Z"
+             transform="translate(-32.03 -26.27)"
+             />
+            <path
+              d="M67.74,43.78V26.42H85.41V79.19H67.91V62.38a4.24,4.24,0,0,0-.52-.57.77.77,0,0,0-.42-.17H50V79.08H32V26.48H49.78v17.3Z"
+              transform="translate(-32.03 -26.27)"
+            />
+            <path
+              d="M103.62,43.79V26.43h53.6c.09,5.62,0,11.41.05,17.36Z"
+              transform="translate(-32.03 -26.27)"
+            />
+            <path
+              d="M103.71,61.71h53.38V78.84c-4.05.69-38.16.91-53.38.31Z"
+              transform="translate(-32.03 -26.27)"
+            />
+          </svg>
+        </a>    
+        <a href="https://fruitensalada.xyz" target="blank" rel="noopener noreferrer">    
+          <img className="fruta" src={fruitensalda} alt="fruitensalda"/>
+        </a>
+      </div>
+
+    </div>
    );
 }
 
